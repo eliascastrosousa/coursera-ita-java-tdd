b91cf10eb53e72c40de73f71071808cd49a241db
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2024-06-13 21:24:21 -0300

    commit

---
+++ camelCase/src/Testes/TesteCamelCase.java
@@ -0,0 +1,15 @@
+package Testes;
+
+import org.junit.jupiter.api.Test;
+
+class TesteCamelCase {
+
+	@Test
+	void testInicializa() {
+		converterCamelCase cc = new converterCamelCase();
+		cc.imprimir();
+	}
+	
+	
+
+}
---
+++ camelCase/src/Testes/converterCamelCase.java
@@ -0,0 +1,26 @@
+package Testes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+;
+
+public class converterCamelCase {
+	
+	private String string;
+	
+	public static List<String> converterCamelCase(String original) {
+		String palavras = new ArrayList<>();
+		palavras = original;
+		return palavras;
+	}
+	
+	
+
+	public void imprimir() {
+		System.out.println(string); 
+	}
+
+	
+	
+}
